668bda17c0578481a35a32695a688ee1eab76c4f
Sapphie <sapphie132@protonmail.com>
2023-02-22 22:08:53 +0100

    Get full rendering

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,6 @@ use gl::types::{GLchar, GLenum, GLfloat, GLint, GLuint};
 use sdl2::event::Event;
 use sdl2::keyboard::{Keycode, Mod};
 
-use image::{self, DynamicImage};
 use std::ffi::CString;
 use std::fs::read_to_string;
 use std::mem::{self, size_of, size_of_val};
@@ -30,8 +29,6 @@ macro_rules! log_err {
         }
     };
 }
-
-const DEFAULT_FONT: &str = "fonts/bitstream-vera-sans-mono-fonts/VeraMono.ttf";
 
 macro_rules! gl_err {
     ($id:path, $iv_fun:path, $info_fun:path, $pname:path) => {
@@ -76,7 +73,6 @@ const VS_SRC_PATH: &str = "src/shaders/vertex.glsl";
 const FS_SRC_PATH: &str = "src/shaders/fragment.glsl";
 
 pub fn main() {
-    let font_path = DEFAULT_FONT;
     let font_desc = FontDesc::new(
         "vera",
         Style::Description {
@@ -102,7 +98,7 @@ pub fn main() {
         .build()
         .unwrap();
 
-    let ctx = window.gl_create_context().unwrap();
+    let _ctx = window.gl_create_context().unwrap();
     gl::load_with(|name| video_subsystem.gl_get_proc_address(name) as *const _);
 
     let mut event_pump = sdl_context.event_pump().unwrap();
@@ -122,38 +118,10 @@ pub fn main() {
     let shader = Shader::new();
 
     // let img = image::open("A.png").unwrap().to_rgba8();
-    let glyph_key = GlyphKey {
-        character: 'A',
-        font_key,
-        size: Size::new(1000.),
-    };
-    let glyph = rast.get_glyph(glyph_key).unwrap();
+
     // setup program
     unsafe {
-        let top = glyph.top as f32;
-        let left = glyph.left as f32;
-        let width = glyph.width as f32;
-        let height = glyph.height as f32;
-
-        let (win_width, win_height) = window.drawable_size();
-        let sx = 1.0 / win_width as f32;
-        let sy = 1.0 / win_height as f32;
-
-        let x1 = left * sx;
-        let w = width * sx;
-        let x2 = x1 + w;
-
-        let y2 = top * sx;
-        let h = height * sy;
-        let y1 = y2 - h;
         // Safe code, but the variables aren't needed outside this block
-        let vertices: [GLfloat; 32] = [
-            //positions      // colours     // texture coordinates
-            x2, y2, 0.0, 1.0, 0.0, 0.0, 1., 0., // top right
-            x2, y1, 0.0, 0.0, 1.0, 0.0, 1., 1., // bottom right
-            x1, y1, 0.0, 0.0, 0.0, 1.0, 0., 1., // bottom left
-            x1, y2, 0.0, 1.0, 1.0, 0.0, 0., 0., // top left
-        ];
 
         let indices = [
             1, 2, 3, // Second triangle
@@ -166,12 +134,6 @@ pub fn main() {
         gl::BindVertexArray(vao);
 
         gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
-        gl::BufferData(
-            gl::ARRAY_BUFFER,
-            size_of_val(&vertices) as isize,
-            mem::transmute(&vertices),
-            gl::STATIC_DRAW,
-        );
 
         gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, ebo);
         gl::BufferData(
@@ -213,6 +175,9 @@ pub fn main() {
             mem::transmute(6 * size_of::<GLfloat>()),
         );
         gl::EnableVertexAttribArray(2);
+
+        gl::Enable(gl::BLEND);
+        gl::BlendFunc(gl::SRC_ALPHA, gl::ONE_MINUS_SRC_ALPHA);
     };
 
     // setup glyph atlas
@@ -227,21 +192,6 @@ pub fn main() {
         // filtering params
         gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MIN_FILTER, gl::LINEAR as i32);
         gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::LINEAR as i32);
-
-        let (pixels, fmt) = get_vec_format(glyph.buffer);
-
-        gl::TexImage2D(
-            gl::TEXTURE_2D,
-            0,
-            gl::RGBA as i32,
-            glyph.width as i32,
-            glyph.height as i32,
-            0,
-            fmt,
-            gl::UNSIGNED_BYTE,
-            pixels.as_ptr() as *const _,
-        );
-        gl::GenerateMipmap(gl::TEXTURE_2D);
 
         shader.r#use();
         shader.set_int("texture1", 0);
@@ -312,13 +262,18 @@ pub fn main() {
             gl::ClearColor(0.2, 0.3, 0.3, 1.);
             gl::Clear(gl::COLOR_BUFFER_BIT);
 
-            gl::ActiveTexture(gl::TEXTURE0);
-            gl::BindTexture(gl::TEXTURE_2D, texture1);
-
             shader.r#use();
 
-            gl::BindVertexArray(vao);
-            gl::DrawElements(gl::TRIANGLES, 6, gl::UNSIGNED_INT, ptr::null());
+            render_text(
+                &state.text_buffer,
+                &mut rast,
+                font_key,
+                -0.5,
+                0.,
+                texture1,
+                window.drawable_size(),
+                vao,
+            );
             let (width, height) = window.drawable_size();
             gl::Viewport(0, 0, width as i32, height as i32);
             // gl::Clear(gl::COLOR_BUFFER_BIT);
@@ -339,70 +294,106 @@ pub fn main() {
     }
 }
 
+fn render_char(
+    c: char,
+    drawable_size: (u32, u32),
+    rast: &mut Rasterizer,
+    font_key: FontKey,
+    texture1: GLuint,
+    vao: GLuint,
+    x0: f32,
+    y0: f32,
+) -> (f32, f32) {
+    let glyph_key = GlyphKey {
+        character: c,
+        font_key,
+        size: Size::new(25.),
+    };
+    let glyph = rast.get_glyph(glyph_key).unwrap();
+    let top = glyph.top as f32;
+    let left = glyph.left as f32;
+    let width = glyph.width as f32;
+    let height = glyph.height as f32;
+
+    let (win_width, win_height) = drawable_size;
+    let sx = 2.0 / win_width as f32;
+    let sy = 2.0 / win_height as f32;
+
+    let x1 = x0 + left * sx;
+    let w = width * sx;
+    let x2 = x1 + w;
+
+    let y2 = y0 + top * sy;
+    let h = height * sy;
+    let y1 = y2 - h;
+    let vertices: [GLfloat; 32] = [
+        //positions      // colours     // texture coordinates
+        x2, y2, 0.0, 1.0, 0.0, 0.0, 1., 0., // top right
+        x2, y1, 0.0, 0.0, 1.0, 0.0, 1., 1., // bottom right
+        x1, y1, 0.0, 0.0, 0.0, 1.0, 0., 1., // bottom left
+        x1, y2, 0.0, 1.0, 1.0, 0.0, 0., 0., // top left
+    ];
+    unsafe {
+        gl::BindVertexArray(vao);
+        gl::BufferData(
+            gl::ARRAY_BUFFER,
+            size_of_val(&vertices) as isize,
+            mem::transmute(&vertices),
+            gl::STATIC_DRAW,
+        );
+
+        let (pixels, fmt) = {
+            let buf = glyph.buffer;
+            let v = match buf {
+                BitmapBuffer::Rgb(v) => v
+                    .chunks_exact(3)
+                    .flat_map(|chunk| {
+                        let avg = chunk.iter().map(|c| *c as u16).sum::<u16>() / 3;
+                        [0xff, 0xff, 0xff, avg as u8]
+                    })
+                    .collect(),
+                BitmapBuffer::Rgba(v) => v,
+            };
+
+            (v, gl::RGBA)
+        };
+
+        gl::ActiveTexture(gl::TEXTURE0);
+        gl::BindTexture(gl::TEXTURE_2D, texture1);
+
+        gl::TexImage2D(
+            gl::TEXTURE_2D,
+            0,
+            gl::RGBA as i32,
+            glyph.width as i32,
+            glyph.height as i32,
+            0,
+            fmt,
+            gl::UNSIGNED_BYTE,
+            pixels.as_ptr() as *const _,
+        );
+        // gl::GenerateMipmap(gl::TEXTURE_2D);
+        gl::DrawElements(gl::TRIANGLES, 6, gl::UNSIGNED_INT, ptr::null());
+    }
+
+    let (ax, ay) = glyph.advance;
+    let next_x0 = x0 + ax as f32 * sx;
+    let next_y0 = y0 + ay as f32 * sx;
+    (next_x0, next_y0)
+}
+
 fn render_text(
     text: &str,
     rast: &mut Rasterizer,
     font_key: FontKey,
-    mut x: f32,
-    mut y: f32,
-    sx: f32,
-    sy: f32,
+    mut x0: f32,
+    mut y0: f32,
+    texture1: GLuint,
+    drawable_size: (u32, u32),
+    vao: GLuint,
 ) {
     for c in text.chars() {
-        let glyph_key = GlyphKey {
-            character: c,
-            font_key,
-            size: Size::new(1.),
-        };
-        let glyph = rast.get_glyph(glyph_key).unwrap();
-        let (buf, format) = match glyph.buffer {
-            BitmapBuffer::Rgb(v) => (v, gl::RGB),
-            BitmapBuffer::Rgba(v) => (v, gl::RGBA),
-        };
-        unsafe {
-            gl::TexImage2D(
-                gl::TEXTURE_2D,
-                0,
-                gl::RED as i32,
-                glyph.width,
-                glyph.height,
-                0,
-                format,
-                gl::UNSIGNED_BYTE,
-                buf.as_ptr() as *const _,
-            )
-        }
-
-        let x2 = x + glyph.left as f32 * sx;
-        let y2 = -y - glyph.top as f32 * sy;
-        let w = glyph.width as f32 * sx;
-        let h = glyph.height as f32 * sy;
-        for (cnt, byte) in buf.iter().enumerate() {
-            if cnt % glyph.width as usize == glyph.width as usize - 1 {
-                println!();
-            }
-            print!("{byte} ");
-        }
-        let coord_box = [
-            [x2, -y2, 0., 0.],
-            [x2 + w, -y2, 1., 0.],
-            [x2, -y2 - h, 0., 1.],
-            [x2 + w, -y2 - h, 1., 1.],
-        ];
-
-        unsafe {
-            gl::BufferData(
-                gl::ARRAY_BUFFER,
-                size_of_val(&coord_box) as isize,
-                mem::transmute(&coord_box),
-                gl::DYNAMIC_DRAW,
-            );
-            gl::DrawArrays(gl::TRIANGLE_STRIP, 0, 4);
-        }
-
-        let (ax, ay) = glyph.advance;
-        x += (ax as f32 / 64.) * sx;
-        y += (ay as f32 / 64.) * sy;
+        (x0, y0) = render_char(c, drawable_size, rast, font_key, texture1, vao, x0, y0)
     }
 }
 
@@ -449,18 +440,6 @@ impl Shader {
             gl::Uniform1i(gl::GetUniformLocation(self.0, string.as_ptr()), val);
         }
     }
-}
-
-fn get_vec_format(buf: BitmapBuffer) -> (Vec<u8>, GLuint) {
-    let v = match buf {
-        BitmapBuffer::Rgb(v) => v
-            .chunks_exact(3)
-            .flat_map(|chunk| chunk.iter().copied().chain(Some(255)))
-            .collect(),
-        BitmapBuffer::Rgba(v) => v,
-    };
-
-    (v, gl::RGBA)
 }
 
 impl Drop for Shader {
